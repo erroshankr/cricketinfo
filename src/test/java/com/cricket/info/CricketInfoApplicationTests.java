37b1e6727b73f4df47fe788eb4bd32451dad3a9b
erroshankr <roshankumarjha24@gmail.com>
2026-01-11 13:36:00 +0530

    Iniial changes

---
+++ src/test/java/com/cricket/info/CricketInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricket.info;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricketInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
